5c25db1443ec4784f00d0a20f633a19a852471fc
Martin Geno <geno@fireorbit.de>
2015-12-31 23:47:59 +0100

    fix

--- public/config_example.js
+++ public/config_example.js
@@ -2,7 +2,7 @@ angular.module('config', [])
 	.factory('config',function(){
 		return {
 			"HOST":"http://localhost:8080",
-			"RRD":"file:///tmp/data",
+			"RRD":"http://localhost:8081/data",
 			"offlineTime":5*60*1000
 			}
 	})
